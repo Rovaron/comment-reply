ac40357a248379cae14694fcf1226585d6aa9a63
Rovaron <rovaron@souzaferro.com.br>
2016-12-21 17:37:15 -0200

    vue-material + base-layout

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+require('./stylus/index.styl')
+
+Vue.use(VueMaterial)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app'
+})
