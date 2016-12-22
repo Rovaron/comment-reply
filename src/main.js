30efd5890ab41221dd56118d3c6af626a721876b
Rovaron <rovaron@souzaferro.com.br>
2016-12-22 19:24:06 -0200

    firebase config + vuefire

--- src/main.js
+++ src/main.js
@@ -3,11 +3,50 @@
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
+import firebase from 'firebase'
+import firebaseui from 'firebaseui'
+import VueFire from 'vuefire'
 require('./stylus/index.styl')
 
 Vue.use(VueMaterial)
+Vue.use(VueFire)
+
+Vue.material.registerTheme({
+  default: {
+    primary: 'blue',
+    accent: 'red'
+  },
+  green: {
+    primary: 'green',
+    accent: 'pink'
+  },
+  orange: {
+    primary: 'orange',
+    accent: 'green'
+  }
+})
+const config = {
+  apiKey: 'AIzaSyAA-XStL2qvvMywnYthiqDwit6M1rULvmE',
+  authDomain: 'eagle-comment-reply.firebaseapp.com',
+  databaseURL: 'https://eagle-comment-reply.firebaseio.com',
+  storageBucket: 'eagle-comment-reply.appspot.com',
+  messagingSenderId: '919095980567'
+}
+const firebaseApp = firebase.initializeApp(config)
+const db = firebaseApp.database()
+console.log(db)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    selectedPage: 'Escolha',
+    sendInboxMessage: false,
+    authData: ''
+  },
+  methods: {
+    login () {
+      window.alert('teste')
+    }
+  }
 })
