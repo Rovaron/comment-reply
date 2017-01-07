967cbf05d504ce023f98a3669db36aeb8b2e93c7
Guilherme Rovaron <Guilherme Rovaron>
2017-01-07 14:54:41 -0200

    Replying proper comments with selected page aeHOOO

--- src/main.js
+++ src/main.js
@@ -41,7 +41,9 @@ new Vue({
   el: '#app',
   data: {
     selectedPage: '',
+    selectedPageToken: '',
     keywordsSeparatedByComma: '',
+    commentReplyText: '',
     user: {},
     logado: false,
     pageList: [],
@@ -67,6 +69,9 @@ new Vue({
     keywordRegex () {
       let keywordsNormalized = this.keywordsSeparatedByComma.replace(/\s/g, '').toLowerCase().replace(/,/g, '|')
       return new RegExp(keywordsNormalized)
+    },
+    commentsToReplyNumber () {
+      return this.commentListWithKeyword.length()
     }
   },
 
@@ -77,14 +82,13 @@ new Vue({
         let accessToken = this.$localStorage.get('token')
         axios.get('https://graph.facebook.com/me/accounts', {
           params: {
-            fields: 'username,name',
+            fields: 'username,name,access_token',
             access_token: accessToken
           }
         }).then(
           (response) => {
             this.logado = true
             this.pageList = response.data.data
-            console.log('pages', this.pageList)
           },
           (error) => {
             console.log('erro pages', error)
@@ -101,6 +105,7 @@ new Vue({
       provider.addScope('manage_pages')
       provider.addScope('publish_pages')
       provider.addScope('pages_messaging')
+      provider.addScope('publish_actions')
 
       firebase.auth().signInWithPopup(provider).then(
         (result) => {
@@ -134,6 +139,7 @@ new Vue({
       let pageId = urlSplited[0]
       let postId = isNumeric(urlSplited[_len - 1]) ? urlSplited[_len - 1] : urlSplited[_len - 2]
       let selectedPage = this.pageList[this.selectedPage]
+      this.selectedPageToken = selectedPage.access_token
 
       if (urlSplited[1] === 'posts' && !urlSplited[_len - 1].includes('_')) {
         postId = selectedPage.id + '_' + postId
@@ -163,10 +169,41 @@ new Vue({
       )
     },
 
+    commentResolved (comment) {
+      let firstName = comment.from.name.split(' ')[0]
+      let resolvedComment = this.commentReplyText.replace(/{nome}/g, firstName)
+      return resolvedComment
+    },
+
+    replyComments () {
+      let promises = []
+      let token = this.selectedPageToken
+      this.commentListWithKeyword.forEach((comment) => {
+        let url = 'https://graph.facebook.com/' + comment.id + '/comments'
+        let message = this.commentResolved(comment)
+        promises.push(axios.post(url, {
+          message: message, access_token: token
+        }))
+      })
+
+      axios.all(promises).then((results) => {
+        results.forEach((response, index) => {
+          if (response.data.id) {
+            let id = response.config.url.split('/')[3]
+            // TODO store ID in firebase;
+          } else {
+            console.log('erro ao responder comentário', response)
+          }
+        })
+      })
+    },
+
     debugging () {
       console.log('comentarios com palavra chave', this.commentListWithKeyword)
       console.log('keywordList', this.keywordRegex)
       console.log('uid', this.user.uid)
+      console.log('user', this.user)
+      this.replyComments()
     }
   }
 })
